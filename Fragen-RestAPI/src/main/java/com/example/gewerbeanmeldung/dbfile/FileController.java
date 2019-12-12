6b15e576e4a306cdbe1e4b2e168a9155265b4ce1
MoritzSch98 <moritz@adviist.com>
2019-12-12 17:06:52 +0100

    test

--- Fragen-RestAPI/src/main/java/com/example/gewerbeanmeldung/dbfile/FileController.java
+++ Fragen-RestAPI/src/main/java/com/example/gewerbeanmeldung/dbfile/FileController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+ 
 @RestController
 public class FileController {
 
